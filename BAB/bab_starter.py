d674f8902b0c8cf1eb8ad168b1c88f6ecf5c3ce2
csnow <colinmsnow@gmail.com>
2021-03-25 20:14:25 -0400

    Works on most cases but has some error

--- BAB/bab_starter.py
+++ BAB/bab_starter.py
@@ -28,7 +28,8 @@ class BBTreeNode():
         Bulids the initial Picos problem
         '''
         prob=pic.Problem()
-   
+
+        # print(self.constraints)
         prob.add_list_of_constraints(self.constraints)    
         
         prob.set_objective('max', self.objective)
@@ -62,6 +63,14 @@ class BBTreeNode():
         return n2
 
 
+
+    def first_noninteger(self):
+        for v in self.vars[:-1]:
+            if v.value == None or abs(round(v.value) - float(v.value)) > 1e-4 :
+                return v
+        return None
+
+
     def bbsolve(self):
         '''
         Use the branch and bound method to solve an integer program
@@ -72,6 +81,13 @@ class BBTreeNode():
               bestnode_vars = the list of variables that create bestres
         '''
 
+
+        # print(self)
+        # print(self.vars)
+        # print(self.constraints)
+        # print(self.prob)
+        # quit()
+
         # these lines build up the initial problem and adds it to a heap
         root = self
         res = root.buildProblem().solve(solver='cvxopt')
@@ -79,8 +95,115 @@ class BBTreeNode():
         bestres = -1e20 # a small arbitrary initial best objective value
         bestnode_vars = root.vars # initialize bestnode_vars to the root vars
 
+        # print(res)
+        # print(res.value)
+        # print(root.vars)
+        # print(heap)
+
         #TODO: fill this part in
 
-        
+        if self.is_integral():
+            return res.value, bestnode_vars
+
+        branch_var = self.first_noninteger()
+
+        # print(branch_var)
+
+        # print("BRANCH VAR" + str(type(branch_var)))
+
+        res, node_vars = recursive_solve(self.branch_floor(branch_var), bestres, bestnode_vars)
+        res2, node_vars2 = recursive_solve(self.branch_ceil(branch_var), bestres, bestnode_vars)
+
+
+        if res > bestres:
+            bestres = res
+            bestnode_vars = node_vars
+
+        if res2 > bestres:
+            bestres = res2
+            bestnode_vars = node_vars2
+        # res2, node_vars2 = recursive_solve(self, bestres, bestnode_vars)
+
+        # print("CONSTRAINTS")
+        # print(self.constraints)
+        # print("FLOOR CONSTRAINTS")
+        # new_obj = self.branch_floor(branch_var).constriants
+        # print(new_obj.constraints)
+
+
+
+
+
+
+
+        # recursive_search(root, )
+
+
+
+
+
+        # quit()
+        print(bestres, bestnode_vars)
         return bestres, bestnode_vars
+
+def recursive_solve(obj, bestres, bestnode_vars):
+
+    # soln = obj.buildProblem().solve(solver='cvxopt')
+    try:
+        soln = obj.prob.solve(solver='cvxopt')
+
+    except pic.modeling.problem.SolutionFailure:
+        # no valid solution
+        return 0, bestnode_vars
+    # print(soln)
+    # print(soln.value)
+    # print(obj.vars)
+
+    if obj.is_integral(): # If everything is integer we are done
+        # print("IS INTEGRAL")
+        bestres = int(soln.value)
+        bestnode_vars = [int(i) for i in obj.vars]
+        return bestres, bestnode_vars
+
+    if soln.value > bestres: # don't bother if the solution is worse
+
+        branch_var = obj.first_noninteger()
+        # print("BRANCH VAR")
+        # print(branch_var)
+
+        res, node_vars = recursive_solve(obj.branch_floor(branch_var), bestres, bestnode_vars)
+        res2, node_vars2 = recursive_solve(obj.branch_ceil(branch_var), bestres, bestnode_vars)
+
+        if res > bestres:
+            bestres = res
+            bestnode_vars = node_vars
+
+        if res2 > bestres:
+            bestres = res2
+            bestnode_vars = node_vars2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # for i in obj.vars:
+        #     print(i)
+    # print("SOLUTION")
+    # print(soln)
+
+    # quit()
+
+    return bestres, bestnode_vars
+
+
  
